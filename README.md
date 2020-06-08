4272b436e304db6e06b4c7736c3ac73b36b9eae6
feloeht <theo@lefevre.bzh>
2020-06-08 20:07:09 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# PowerShell Password Generator
+
+A simple PowerShell secure random password generator.
+## Usage
+
+Use either CMD or Powershell to generate a 8 characters password
+
+```shell
+./simple8.ps1
+```
+
+Generate shorter or longer password
+
+```shell
+./pass.ps1 <length>
+```
+Parameter : <length> - length of the generated password \
+Default : 8 characters password
+## Meet Complexity Requirements policy
+
+ Lowercase letters, uppercase letters, numbers, and symbols.
--- pass.ps1
+++ pass.ps1
@@ -1,12 +1,30 @@
+param($length)
+
 function Get-RandomCharacters($length, $characters) {
+    
     $random = 1..$length | ForEach-Object { Get-Random -Maximum $characters.length }
     $private:ofs=""
     return [String]$characters[$random]
 }
 
-$password = Get-RandomCharacters -length 5 -characters 'abcdefghiklmnoprstuvwxyz'
-$password += Get-RandomCharacters -length 1 -characters 'ABCDEFGHKLMNOPRSTUVWXYZ'
-$password += Get-RandomCharacters -length 1 -characters '1234567890'
-$password += Get-RandomCharacters -length 1 -characters '!"§$%&/()=?}][{@#*+'
+function Get-EightCharactersPassword() {
+    $password = Get-RandomCharacters -length 5 -characters 'abcdefghiklmnoprstuvwxyz'
+    $password += Get-RandomCharacters -length 1 -characters '!@#$%&_-+=\(){}:;"<>?/'
+    $password += Get-RandomCharacters -length 1 -characters 'ABCDEFGHKLMNOPRSTUVWXYZ'
+    $password += Get-RandomCharacters -length 1 -characters '1234567890'
+    return $password
+}
 
-return $password
+if($length -eq $null -or $length -eq 8){
+    return Get-EightCharactersPassword
+}
+elseif($length > 9){
+    $characters = $characters = 'abcdefghiklmnoprstuvwxyz1234567890!@#$%&_-+=\(){}:;"<>?/ABCDEFGHKLMNOPRSTUVWXYZ'
+    $password = Get-EightCharactersPassword
+    $password += Get-RandomCharacters -length $length-8 -characters $characters
+    return $password
+}
+else{
+    $characters = $characters = 'abcdefghiklmnoprstuvwxyz1234567890!@#$%&_-+=\(){}:;"<>?/ABCDEFGHKLMNOPRSTUVWXYZ'
+    return Get-RandomCharacters -length $length -characters $characters
+}
